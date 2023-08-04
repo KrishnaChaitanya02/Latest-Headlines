7d133df8e2e8d0601fec4f8486ea2836bd237238
KrishnaChaitanya02 <141296924+KrishnaChaitanya02@users.noreply.github.com>
2023-08-04 12:37:32 +0530

    Add files via upload

---
+++ Project Files/app/src/main/java/com/example/latestheadlines/Articles.kt
@@ -0,0 +1,7 @@
+package com.example.latestheadlines
+import com.google.gson.annotations.SerializedName
+data class Articles(
+    @SerializedName("title"       ) var title       : String? = null,
+    @SerializedName("description" ) var description : String? = null,
+    @SerializedName("urlToImage"  ) var urlToImage  : String? = null,
+)
